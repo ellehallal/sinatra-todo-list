15db5d8b2f1bb149b30945ef6b933518a495b71f
Elle H <itsellej@gmail.com>
2018-11-30 18:56:08 +0000

    Test passed: creates an instance of NewToDoItem and makes it into a hash

--- lib/newtodoitem.rb
+++ lib/newtodoitem.rb
@@ -1,9 +1,16 @@
 class NewToDoItem
   attr_reader :todo, :completed
-  
+
   def initialize(todo)
     @todo = todo
     @completed = false
+  end
+
+  def create_todo
+    {
+      todo: @todo,
+      completed: @completed
+    }
   end
 
 
--- spec/newtodoitem_spec.rb
+++ spec/newtodoitem_spec.rb
@@ -3,12 +3,12 @@ require 'newtodoitem'
 RSpec.describe NewToDoItem do
 
   it "creates an instance of NewToDoItem" do
-    newtodoitem = NewToDoItem.new('wash clothes')
+    newtodoitem = NewToDoItem.new("wash clothes")
     expect(newtodoitem.is_a?Object).to eq(true)
   end
 
-  it "creates an instance of NewToDoItem and makes it into a " do
-    newtodoitem = NewToDoItem.new('wash clothes')
-    expect(newtodoitem.is_a?Object).to eq(true)
+  it "creates an instance of NewToDoItem and makes it into a hash" do
+    newtodoitem = NewToDoItem.new("wash clothes")
+    expect(newtodoitem.create_todo).to eq({todo: "wash clothes", completed: false})
   end
 end
