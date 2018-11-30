a8a9fc88dfcad472b3404fe36df1a8e9431d95b6
Elle H <itsellej@gmail.com>
2018-11-30 19:31:28 +0000

    Add show_all_todos, show_completed_todos, show_incomplete_todos methods

--- lib/todolistactions.rb
+++ lib/todolistactions.rb
@@ -19,7 +19,35 @@ class TodoListActions
   end
 
   def mark_as_complete(index)
-    todo_list[index][:completed] = true  
+    todo_list[index][:completed] = true
+  end
+
+  def show_all_todos
+    todo_list.each do |todo_entry|
+      todo_entry.each do |todo, completed|
+        puts todo
+      end
+    end
+  end
+
+  def show_completed_todos
+    todo_list.each do |todo_entry|
+      todo_entry.each do |todo, completed|
+        if todo_entry[:completed] = true
+          puts todo
+        end
+      end
+    end
+  end
+
+  def show_incomplete_todos
+    todo_list.each do |todo_entry|
+      todo_entry.each do |todo, completed|
+        if todo_entry[:completed] = false
+          puts todo
+        end
+      end
+    end
   end
 
 end
